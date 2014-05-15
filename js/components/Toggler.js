ec209a173762fa3d616e19176d9f0f3810c5e94c
Jon Wyatt <Jon.Wyatt@moneyadviceservice.org.uk>
2014-05-15 12:47:08 +0100

    Remove redundant closure. Re-indentation

--- js/components/Toggler.js
+++ js/components/Toggler.js
@@ -27,96 +27,93 @@
  * @return {[type]}           [description]
  * @private
  */
-define(['jquery', 'MASModule', 'eventsWithPromises'], function($, MASModule, eventsWithPromises) {
+define(['jquery', 'MASModule', 'eventsWithPromises'], function ($, MASModule, eventsWithPromises) {
   'use strict';
 
-  return (function() {
-    // Class variables
-    var TogglerProto;
+  // Class variables
+  var TogglerProto;
 
-    function Toggler($el) {
-      this.setElement($el);
-      this.attrs = ['toggler'];
+  function Toggler($el) {
+    this.setElement($el);
+    this.attrs = ['toggler'];
 
-      this.init();
+    this.init();
 
-      return this;
+    return this;
+  }
+
+  /**
+   * Inherit from base module, for shared methods and interface
+   * @type {[type]}
+   * @private
+   */
+  TogglerProto = Toggler.prototype = new MASModule();
+
+  /**
+   * Init function
+   * @return {Toggler}
+   */
+  TogglerProto.init = function () {
+    this.$target = $(this.attr('toggler'));
+    this.isShown = !!this.$target.hasClass('show'); // is the target element visible already
+
+    return this.setListeners(true);
+  };
+
+  /**
+   * Bind or unbind relevant DOM events
+   * @param {Boolean} isActive Set to 'true' to bind to events, 'false' to unbind.
+   */
+  TogglerProto.setListeners = function (isActive) {
+    this.$el[isActive ? 'on' : ' off']('click', $.proxy(function (e) {
+      this.toggle();
+
+      e.preventDefault();
+    }, this));
+
+    return this;
+  };
+
+  /**
+   * Toggle the element
+   * @param  {[type]} forceTo Supply 'show' or 'hide' to
+   * explicitly set, otherwise will automatically toggle
+   * @return {[type]}         [description]
+   */
+  TogglerProto.toggle = function (forceTo) {
+    var func;
+
+    // is there an override parameter?
+    if (typeof forceTo !== 'undefined') {
+      func = forceTo === 'show' ? 'addClass' : 'removeClass';
+    }
+    else {
+      func = this.isShown ? 'removeClass' : 'addClass';
     }
 
-    /**
-     * Inherit from base module, for shared methods and interface
-     * @type {[type]}
-     * @private
-     */
-    TogglerProto = Toggler.prototype = new MASModule();
+    // toggle the element
+    this.isShown = !!this.$target[func]('show').hasClass('show');
 
-    /**
-     * Init function
-     * @return {Toggler}
-     */
-    TogglerProto.init = function() {
-      this.$target = $(this.attr('toggler'));
-      this.isShown = !!this.$target.hasClass('show'); // is the target element visible already
+    // toggle the class on the trigger element (active = shown / nothing = not shown)
+    this.$el[func]('active');
 
-      return this.setListeners(true);
-    };
+    // can bind to this by eventsWithPromises.subscribe('toggler:toggled', function(Toggler) { });
+    if (typeof forceTo === 'undefined') {
+      eventsWithPromises.publish('toggler:toggled', {
+        emitter: this
+      });
+    }
 
-    /**
-     * Bind or unbind relevant DOM events
-     * @param {Boolean} isActive Set to 'true' to bind to events, 'false' to unbind.
-     */
-    TogglerProto.setListeners = function(isActive) {
-      this.$el[isActive ? 'on' : ' off']('click', $.proxy(function(e) {
-        this.toggle();
+    return this;
+  };
 
-        e.preventDefault();
-      }, this));
+  /**
+   * Public method to unbind all events this instance bound.
+   * @return {[type]}
+   */
+  TogglerProto.destroy = function () {
+    return this.setListeners(false);
+  };
 
-      return this;
-    };
-
-    /**
-     * Toggle the element
-     * @param  {[type]} forceTo Supply 'show' or 'hide' to
-     * explicitly set, otherwise will automatically toggle
-     * @return {[type]}         [description]
-     */
-    TogglerProto.toggle = function(forceTo) {
-      var func;
-
-      // is there an override parameter?
-      if (typeof forceTo !== 'undefined') {
-        func = forceTo === 'show' ? 'addClass' : 'removeClass';
-      }
-      else {
-        func = this.isShown ? 'removeClass' : 'addClass';
-      }
-
-      // toggle the element
-      this.isShown = !!this.$target[func]('show').hasClass('show');
-
-      // toggle the class on the trigger element (active = shown / nothing = not shown)
-      this.$el[func]('active');
-
-      // can bind to this by eventsWithPromises.subscribe('toggler:toggled', function(Toggler) { });
-      if (typeof forceTo === 'undefined') {
-        eventsWithPromises.publish('toggler:toggled', {
-          emitter: this
-        });
-      }
-
-      return this;
-    };
-
-    /**
-     * Public method to unbind all events this instance bound.
-     * @return {[type]}
-     */
-    TogglerProto.destroy = function() {
-      return this.setListeners(false);
-    };
-
-    return Toggler;
-
-  }());
+  return Toggler;
 });
