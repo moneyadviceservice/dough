e9f8c68e9aeccb15f30f6454bcc07b99a4f89590
Jon Wyatt <Jon.Wyatt@moneyadviceservice.org.uk>
2014-06-13 09:56:40 +0100

    Make tabselector tests more readable

--- test/tests/TabSelector_spec.js
+++ test/tests/TabSelector_spec.js
@@ -3,8 +3,9 @@ describe('Tab selector', function () {
   'use strict';
 
   var activeClass = 'is-active',
-      activeTrigger = '[data-mas-tabselector-trigger].' + activeClass,
-      triggers = '[data-mas-tabselector-trigger] a',
+      trigger = '[data-mas-tabselector-trigger]',
+      active = trigger + '.' + activeClass + ' a',
+      triggers = trigger + ' a',
       activeTarget = '[data-mas-tabselector-target].' + activeClass;
 
   beforeEach(function (done) {
@@ -13,55 +14,64 @@ describe('Tab selector', function () {
         ['jquery', 'TabSelector'],
         function ($, TabSelector) {
           self.$html = $(window.__html__['test/fixtures/TabSelector.html']);
-          self.$triggerContainer = self.$html.find('[data-mas-tabselector-triggers]');
+          self.$menu = self.$html.find('[data-mas-tabselector-triggers]');
           self.tabSelector = new TabSelector(self.$html);
           self.tabSelector.init();
           done();
         }, done);
   });
 
-  afterEach(function(){
-    this.componentLoader = null;
-  });
+  function isOpen($menu){
+    return $menu.hasClass(activeClass);
+  }
+
+  function activeTrigger($menu) {
+    return $menu.find(active);
+  }
+
+  function activeTargetText($root) {
+    return $root.find(activeTarget).text();
+  }
 
   it('selects the first item in the list', function() {
-    expect(this.$html.find(activeTrigger + ' a').html()).to.equal('Show panel 1');
+    expect(this.$html.find(active).html()).to.equal('Show panel 1');
   });
 
   it('replaces the currently selected item', function() {
-    this.$triggerContainer.find(triggers).last().click();
-    this.$triggerContainer.find(triggers).eq(1).click();
-    expect(this.$triggerContainer.find(activeTrigger + ' a').html()).to.equal('Show panel 2');
-    expect(this.$html.find(activeTrigger).length).to.equal(2);
+    this.$menu.find(triggers).last().click();
+    this.$menu.find(triggers).eq(1).click();
+    expect(activeTrigger(this.$menu).html()).to.equal('Show panel 2');
+    expect(this.$html.find(active).length).to.equal(2);
   });
 
   it('toggles the menu when the selected item is clicked', function() {
-    this.$triggerContainer.find(activeTrigger + ' a').first().click();
-    expect(this.$triggerContainer.hasClass(activeClass)).to.equal(true);
-    this.$triggerContainer.find(activeTrigger + ' a').first().click();
-    expect(this.$triggerContainer.hasClass(activeClass)).to.equal(false);
+    activeTrigger(this.$menu).click();
+    expect(isOpen(this.$menu)).to.equal(true);
+    activeTrigger(this.$menu).click();
+    expect(isOpen(this.$menu)).to.equal(false);
   });
 
   it('closes the menu when an item on it is clicked', function() {
-    this.$html.find(activeTrigger + ' a').first().click();
-    this.$triggerContainer.find('a').first().click();
-    expect(this.$triggerContainer.hasClass(activeClass)).to.equal(false);
+    activeTrigger(this.$menu).click();
+    this.$menu.find(triggers).first().click();
+    expect(isOpen(this.$menu)).to.equal(false);
   });
 
   it('shows the associated target panel when a trigger is clicked', function() {
-    this.$triggerContainer.find(triggers).last().click();
-    expect(this.$html.find(activeTarget).text()).to.equal('Panel 3');
-    this.$triggerContainer.find(triggers).first().click();
-    expect(this.$html.find(activeTarget).text()).to.equal('Panel 1');
+    this.$menu.find(triggers).last().click();
+    expect(activeTargetText(this.$html)).to.equal('Panel 3');
+    this.$menu.find(triggers).first().click();
+    expect(activeTargetText(this.$html)).to.equal('Panel 1');
   });
 
   it('updates other copies of the clicked trigger', function() {
-    this.$html.find('#panel-2 a:eq(1)').click();
-    expect(this.$triggerContainer.find(triggers).eq(1).parent().hasClass(activeClass)).to.equal(true);
+    var $trigger = this.$html.find('.panel [data-mas-tabselector-trigger="2"]');
+    $trigger.find('a').click();
+    expect(this.$html.find('[data-mas-tabselector-trigger="2"].is-active').length).to.equal(2);
   });
 
   it('doesn\'t open the menu if a trigger outside the menu is clicked', function() {
-    this.$html.find('#panel-2 a:eq(2)').click();
-    expect(this.$triggerContainer.hasClass(activeClass)).to.equal(false);
+    this.$html.find('.panel a:eq(2)').click();
+    expect(isOpen(this.$menu)).to.equal(false);
   });
 });
