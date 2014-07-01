db0ff31ac58e4bda71257896f22a5ed592322e00
Phil Lee <asmega@ph-lee.com>
2014-07-01 12:23:20 +0100

    add bower components to asset paths

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -55,6 +55,8 @@ module Dummy
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
   end
 end
 
