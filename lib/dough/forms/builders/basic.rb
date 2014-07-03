9d72dec6aa1f33c16115f2f32009f96800e48a1c
David Kewal <david.kewal@gmail.com>
2014-07-03 11:37:21 +0100

    First stab at the form builder to build normal inputs using the styleguide guidelines

--- lib/dough/forms/builders/basic.rb
+++ lib/dough/forms/builders/basic.rb
@@ -3,6 +3,12 @@ module Dough
     module Builders
       class Basic < ::ActionView::Helpers::FormBuilder
 
+        def text_field(name, *args)
+          @template.content_tag :div, class: 'form__item' do
+            label(name, class: 'form__label-heading') + @template.tag(:br) + super
+          end
+        end
+
       end
     end
   end
--- spec/lib/dough/forms/builders/basic_spec.rb
+++ spec/lib/dough/forms/builders/basic_spec.rb
@@ -1,9 +1,26 @@
 require 'spec_helper'
+class TestHelper < ActionView::Base; end
 
 module Dough
   module Forms
     module Builders
       describe Basic do
+
+
+        let(:resource)    { User.new }
+        let(:helper)      { TestHelper.new }
+        let(:builder)     { Dough::Forms::Builders::Basic.new(:user, resource, helper, {}, nil) }
+        let(:normal_input) { builder.text_field :name }
+
+        context 'inputs' do
+          it "should build normal inputs" do
+            expect(normal_input).to include '<div class="form__item">'
+            expect(normal_input).to include '<label class="form__label-heading" for="user_name">Name</label>'
+            expect(normal_input).to include '<input id="user_name" name="user[name]" size="30" type="text" />'
+
+          end
+
+        end
 
       end
     end
