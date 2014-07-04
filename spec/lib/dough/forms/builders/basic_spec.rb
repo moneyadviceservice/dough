d4b1977cd8e300fced7497df35d534dc7235f0e0
David Kewal <david.kewal@gmail.com>
2014-07-04 12:59:00 +0100

    Spec to test building required inputs

--- spec/lib/dough/forms/builders/basic_spec.rb
+++ spec/lib/dough/forms/builders/basic_spec.rb
@@ -7,17 +7,26 @@ module Dough
       describe Basic do
 
 
-        let(:resource)    { User.new }
+        let(:resource)      { User.new }
         let(:template)      { TestHelper.new }
-        let(:builder)     { Dough::Forms::Builders::Basic.new(:user, resource, template, {}, nil) }
-        let(:normal_input) { builder.text_field :name }
+        let(:builder)       { Dough::Forms::Builders::Basic.new(:user, resource, template, {}, nil) }
+        let(:normal_input)  { builder.text_field :name }
+        let(:email_input)   { builder.text_field :email, type: 'email' }
 
         context 'inputs' do
           it "should build normal inputs" do
             expect(normal_input).to include '<div class="form__item">'
             expect(normal_input).to include '<label class="form__label-heading" for="user_name">Name</label>'
             expect(normal_input).to include '<input id="user_name" name="user[name]" size="30" type="text" />'
+          end
 
+          describe 'required inputs' do
+            it "should build required inputs based of a model resource" do
+              expect(email_input).to include '<div class="form__item">'
+              expect(email_input).to include '<label class="form__label-heading" for="email">Email *</label>'
+              expect(email_input).to include '<span class="visually-hidden">required</span>'
+              expect(email_input).to include '<input id="email" name="user[email]" size="30" type="email" required="required" aria-required="true" />'
+            end
           end
 
         end
