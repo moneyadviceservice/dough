291466bb89e6ce423b4613d73c9c03280ac5a089
Jon Wyatt <Jon.Wyatt@moneyadviceservice.org.uk>
2014-07-04 12:23:53 +0100

    Javascript code coverage reporter

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,15 +32,22 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.html': ['html2js']
+      '**/*.html': ['html2js'],
+      'js/**/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
+
+    // configure the reporter
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
     // web server port
     port: 9876,
