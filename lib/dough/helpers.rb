7d0953231ed8dcb8a9d2f24e490e00d1ba8bcbee
Phil Lee <asmega@ph-lee.com>
2014-07-11 09:38:19 +0100

    remove dough renderer.
    inset block now has own renderer for now

--- lib/dough/helpers.rb
+++ lib/dough/helpers.rb
@@ -1,18 +1,7 @@
+require "dough/helpers/inset_block"
+
 module Dough
   module Helpers
-    require "dough/helpers/inset_block"
-
-    def dough
-      @dough_renderer ||= Dough::Renderer.new(lookup_context)
-    end
-
-    def lookup_context
-      # May want use whatever is in the controller instead
-      ActionView::LookupContext.new(ActionController::Base.view_paths)
-    end
-  end
-
-  class Renderer < ActionView::Renderer
     def inset_block(text)
       Helpers::InsetBlock.new(renderer: self, text: text).render
     end
--- lib/dough/helpers/inset_block.rb
+++ lib/dough/helpers/inset_block.rb
@@ -3,23 +3,26 @@ module Dough
     class InsetBlock
       include ActionView::Helpers::RenderingHelper
 
-      attr_reader :text, :renderer
+      attr_reader :text
 
       def initialize(options = {})
         @text = options[:text]
-        @renderer = options[:renderer]
       end
 
       def render
-        renderer.render lookup_context, partial: 'inset_block', locals: { text: text }
+        renderer.render lookup_context, { partial: 'inset_block', locals: { text: text } }, {}
       end
 
       private
 
+      def renderer
+        @renderer ||= ActionView::PartialRenderer.new(lookup_context)
+      end
+
       def lookup_context
-        # OBJECT MUTATED? May need to dupe
-        renderer.lookup_context.view_paths << Dough::Engine.root.join('app/views/dough/helpers/inset_block')
-        renderer.lookup_context
+        context = ActionView::LookupContext.new(ActionController::Base.view_paths)
+        context.view_paths << Dough::Engine.root.join('app/views/dough/helpers/inset_block')
+        context
       end
     end
   end
