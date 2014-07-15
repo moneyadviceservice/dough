1ce2fb4e82423cf3bc635bcb397ec018d1202738
Phil Lee <asmega@ph-lee.com>
2014-07-15 17:47:46 +0100

    Revert "inset block partial path is namespaced to dough"
    This reverts commit a42da7f5c225af3610b738204a97b52ec2f2c4fd.

--- lib/dough/helpers/inset_block.rb
+++ lib/dough/helpers/inset_block.rb
@@ -1,15 +1,28 @@
 module Dough
   module Helpers
     class InsetBlock
-      attr_reader :text, :renderer
+      include ActionView::Helpers::RenderingHelper
+
+      attr_reader :text
 
       def initialize(options = {})
         @text = options[:text]
-        @renderer = options[:renderer]
       end
 
       def render
-        renderer.render partial: 'dough/helpers/inset_block/inset_block', locals: { text: text }
+        renderer.render lookup_context, { partial: 'inset_block', locals: { text: text } }, {}
+      end
+
+      private
+
+      def renderer
+        @renderer ||= ActionView::PartialRenderer.new(lookup_context)
+      end
+
+      def lookup_context
+        context = ActionView::LookupContext.new(ActionController::Base.view_paths)
+        context.view_paths << Dough::Engine.root.join('app/views/dough/helpers/inset_block')
+        context
       end
     end
   end
--- spec/lib/dough/helpers/inset_block_spec.rb
+++ spec/lib/dough/helpers/inset_block_spec.rb
@@ -2,28 +2,11 @@ require 'spec_helper'
 
 module Dough
   module Helpers
-    class TestInsetBlockController < AbstractController::Base
-      include AbstractController::Helpers
-      include AbstractController::Rendering
-
-      helper Dough::Helpers
-
-      def index
-        render inline: "<%= inset_block 'hello' %>"
-      end
-
-      private
-
-      def lookup_context
-        ActionView::LookupContext.new(ActionController::Base.view_paths)
-      end
-    end
-
     describe InsetBlock do
+      subject{ described_class.new(text: 'hello') }
+
       it 'renders text' do
-        controller = TestInsetBlockController.new
-        controller.process(:index)
-        expect(controller.response_body).to include('hello')
+        expect(subject.render).to include('hello')
       end
     end
   end
