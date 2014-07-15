bd389f2d02e4197de4ca60bdecbba85e595a94f8
Phil Lee <asmega@ph-lee.com>
2014-07-15 17:47:48 +0100

    Revert "remove dough renderer."
    This reverts commit 7d0953231ed8dcb8a9d2f24e490e00d1ba8bcbee.

--- lib/dough/helpers/inset_block.rb
+++ lib/dough/helpers/inset_block.rb
@@ -3,26 +3,23 @@ module Dough
     class InsetBlock
       include ActionView::Helpers::RenderingHelper
 
-      attr_reader :text
+      attr_reader :text, :renderer
 
       def initialize(options = {})
         @text = options[:text]
+        @renderer = options[:renderer]
       end
 
       def render
-        renderer.render lookup_context, { partial: 'inset_block', locals: { text: text } }, {}
+        renderer.render lookup_context, partial: 'inset_block', locals: { text: text }
       end
 
       private
 
-      def renderer
-        @renderer ||= ActionView::PartialRenderer.new(lookup_context)
-      end
-
       def lookup_context
-        context = ActionView::LookupContext.new(ActionController::Base.view_paths)
-        context.view_paths << Dough::Engine.root.join('app/views/dough/helpers/inset_block')
-        context
+        # OBJECT MUTATED? May need to dupe
+        renderer.lookup_context.view_paths << Dough::Engine.root.join('app/views/dough/helpers/inset_block')
+        renderer.lookup_context
       end
     end
   end
--- spec/lib/dough/helpers/inset_block_spec.rb
+++ spec/lib/dough/helpers/inset_block_spec.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 module Dough
   module Helpers
     describe InsetBlock do
-      subject{ described_class.new(text: 'hello') }
+      let(:context){ ActionView::LookupContext.new(ActionController::Base.view_paths) }
+      let(:renderer){ Renderer.new(context) }
+
+      subject{ described_class.new(renderer: renderer, text: 'hello') }
 
       it 'renders text' do
         expect(subject.render).to include('hello')
