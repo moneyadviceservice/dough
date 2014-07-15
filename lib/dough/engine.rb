67089c804e69f9bbd8d2f63c0e6cf01a1badac6d
Phil Lee <asmega@ph-lee.com>
2014-07-15 17:47:47 +0100

    Revert "load dough rails helpers by default"
    This reverts commit 0163b3eafcd509dd7a79c96c25fdb6b7c0e100cd.

--- lib/dough/engine.rb
+++ lib/dough/engine.rb
@@ -1,9 +1,5 @@
 module Dough
   class Engine < ::Rails::Engine
     isolate_namespace Dough
-
-    config.to_prepare do
-      ::ApplicationController.helper(Helpers)
-    end
   end
 end
--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper Dough::Helpers
 end
