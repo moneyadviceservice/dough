c088a5322c6f8f36f4140094ef37e0e9ef9dab2e
Phil Lee <asmega@ph-lee.com>
2014-07-16 09:56:41 +0100

    Revert "Revert "inset block partial path is namespaced to dough""
    This reverts commit 1ce2fb4e82423cf3bc635bcb397ec018d1202738.

--- lib/dough/helpers.rb
+++ lib/dough/helpers.rb
@@ -3,7 +3,7 @@ require "dough/helpers/inset_block"
 module Dough
   module Helpers
     def inset_block(text)
-      Helpers::InsetBlock.new(text: text).render
+      Helpers::InsetBlock.new(renderer: self, text: text).render
     end
   end
 end
--- lib/dough/helpers/inset_block.rb
+++ lib/dough/helpers/inset_block.rb
@@ -1,28 +1,15 @@
 module Dough
   module Helpers
     class InsetBlock
-      include ActionView::Helpers::RenderingHelper
-
-      attr_reader :text
+      attr_reader :text, :renderer
 
       def initialize(options = {})
         @text = options[:text]
+        @renderer = options[:renderer]
       end
 
       def render
-        renderer.render lookup_context, { partial: 'inset_block', locals: { text: text } }, {}
-      end
-
-      private
-
-      def renderer
-        @renderer ||= ActionView::PartialRenderer.new(lookup_context)
-      end
-
-      def lookup_context
-        context = ActionView::LookupContext.new(ActionController::Base.view_paths)
-        context.view_paths << Dough::Engine.root.join('app/views/dough/helpers/inset_block')
-        context
+        renderer.render partial: 'dough/helpers/inset_block/inset_block', locals: { text: text }
       end
     end
   end
