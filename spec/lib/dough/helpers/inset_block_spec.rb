f4c5a320d3f76e989dab1646312f9330d844764c
Gareth Visagie <gareth@gjvis.com>
2014-07-16 18:24:44 +0100

    Leverage rspec-rails controller specs

--- spec/lib/dough/helpers/inset_block_spec.rb
+++ spec/lib/dough/helpers/inset_block_spec.rb
@@ -2,29 +2,21 @@ require 'spec_helper'
 
 module Dough
   module Helpers
-    class TestInsetBlockController < AbstractController::Base
-      include AbstractController::Helpers
-      include AbstractController::Rendering
-      include ActionView::Rendering
+    describe InsetBlock, type: 'controller' do
+      render_views
 
-      helper Dough::Helpers
+      controller do
+        helper Dough::Helpers
 
-      def index
-        render inline: "<%= inset_block 'hello' %>"
+        def index
+          render inline: "<%= inset_block 'hello' %>"
+        end
       end
 
-      private
+      it 'renders "text"' do
+        get :index
 
-      def lookup_context
-        ActionView::LookupContext.new(ActionController::Base.view_paths)
-      end
-    end
-
-    describe InsetBlock do
-      it 'renders text' do
-        controller = TestInsetBlockController.new
-        controller.process(:index)
-        expect(controller.response_body).to include('hello')
+        expect(response.body).to include('hello')
       end
     end
   end
