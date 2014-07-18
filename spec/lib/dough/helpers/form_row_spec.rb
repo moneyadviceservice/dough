26f9ac712aeb09b5f2bae6d296909024a12699dd
Phil Lee <asmega@ph-lee.com>
2014-07-18 10:32:16 +0100

    refactor make tests look similar

--- spec/lib/dough/helpers/form_row_spec.rb
+++ spec/lib/dough/helpers/form_row_spec.rb
@@ -1,45 +1,30 @@
 require 'spec_helper'
 
-module Dough
+module ActionView
   module Helpers
-    class TestFormRowController < AbstractController::Base
-      include AbstractController::Helpers
-      include AbstractController::Rendering
-      include ActionView::Rendering
+    describe FormBuilder, type: :controller do
+      render_views
 
-      helper Dough::Helpers
+      controller do
+        helper Dough::Helpers
 
-      def protect_against_forgery?
-        false
-      end
-      helper_method :protect_against_forgery?
-
-      def index
-        render inline: "<%= form_for User.new, url: '/' do |f| %>" \
-                       "  <%= f.form_row do %>" \
-                       "    <%= 'hello world' %>" \
-                       "  <% end %>" \
-                       "<% end %>"
+        def index
+          render inline: "<%= form_for User.new, url: '/' do |f| %>" \
+                         "  <%= f.form_row do %>" \
+                         "    <%= 'hello world' %>" \
+                         "  <% end %>" \
+                         "<% end %>"
+        end
       end
 
-      private
-
-      def lookup_context
-        ActionView::LookupContext.new(ActionController::Base.view_paths)
-      end
-    end
-
-    describe TestFormRowController do
       it 'adds form row' do
-        controller = TestFormRowController.new
-        controller.process(:index)
-        expect(controller.response_body).to include('form__row')
+        get :index
+        expect(response.body).to include('form__row')
       end
 
       it 'renders row contents' do
-        controller = TestFormRowController.new
-        controller.process(:index)
-        expect(controller.response_body).to include('hello world')
+        get :index
+        expect(response.body).to include('hello world')
       end
     end
   end
