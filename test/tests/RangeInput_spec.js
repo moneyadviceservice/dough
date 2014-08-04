4f03081297957af9a40c3f1596e574d7bf5fe89a
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2014-08-04 12:51:02 +0100

    Conform to JS whitespace guidelines

--- test/tests/RangeInput_spec.js
+++ test/tests/RangeInput_spec.js
@@ -1,8 +1,8 @@
-describe('Range input', function () {
+describe('Range input', function() {
 
   'use strict';
 
-  before(function (done) {
+  before(function(done) {
     var self = this;
     window.Modernizr = {
       inputtypes: {
@@ -11,7 +11,7 @@ describe('Range input', function () {
     };
     requirejs(
         ['jquery', 'RangeInput', 'featureDetect', 'eventsWithPromises'],
-        function ($, RangeInput, featureDetect, eventsWithPromises) {
+        function($, RangeInput, featureDetect, eventsWithPromises) {
           self.featureDetect = featureDetect;
           self.RangeInput = RangeInput;
           self.eventsWithPromises = eventsWithPromises;
@@ -19,7 +19,7 @@ describe('Range input', function () {
         }, done);
   });
 
-  beforeEach(function(){
+  beforeEach(function() {
     this.$html = $(window.__html__['test/fixtures/RangeInput.html']);
   });
 
@@ -27,9 +27,9 @@ describe('Range input', function () {
     this.$html.remove();
   });
 
-  describe('Range inputs supported', function () {
+  describe('Range inputs supported', function() {
 
-    beforeEach(function () {
+    beforeEach(function() {
       this.featureDetect.inputtypes.range = true;
       this.rangeInput = new this.RangeInput(this.$html);
       this.rangeInput.init();
@@ -38,25 +38,25 @@ describe('Range input', function () {
 
     });
 
-    it('creates a copy of the input and label if the range slider type is supported', function () {
+    it('creates a copy of the input and label if the range slider type is supported', function() {
       expect(this.$html.find('input').length).to.equal(2);
       expect(this.$html.find('label').length).to.equal(2);
     });
 
 
-    it('keeps the slider in sync if the text input changes', function () {
+    it('keeps the slider in sync if the text input changes', function() {
       this.$inputText.val('2000').trigger('change');
       expect(this.$inputSlider.val()).to.equal('2000');
       this.$inputText.val('50000').trigger('change');
       expect(this.$inputSlider.val()).to.equal('5000'); // because range input has a max of 5000
     });
 
-    it('keeps the text input in sync if the slider changes', function () {
+    it('keeps the text input in sync if the slider changes', function() {
       this.$inputSlider.val('2000').trigger('change');
       expect(this.$inputText.val()).to.equal('2000');
     });
 
-    it('publishes an event when the value changes', function () {
+    it('publishes an event when the value changes', function() {
       var spy = sinon.spy();
       this.eventsWithPromises.subscribe('rangeInput:change', spy);
       this.$inputText.val('3000').trigger('change');
@@ -69,9 +69,9 @@ describe('Range input', function () {
   });
 
 
-  describe('Range inputs supported but don\'t keep inputs in sync', function () {
+  describe('Range inputs supported but don\'t keep inputs in sync', function() {
 
-    beforeEach(function () {
+    beforeEach(function() {
       this.featureDetect.inputtypes.range = true;
       this.rangeInput = new this.RangeInput(this.$html, {
         keepSynced: false
@@ -82,15 +82,15 @@ describe('Range input', function () {
 
     });
 
-    it('creates a copy of the input and label if the range slider type is supported', function () {
+    it('creates a copy of the input and label if the range slider type is supported', function() {
       expect(this.$html.find('input').length).to.equal(2);
       expect(this.$html.find('label').length).to.equal(2);
     });
   });
 
-  describe('Range inputs not supported', function () {
+  describe('Range inputs not supported', function() {
 
-    beforeEach(function () {
+    beforeEach(function() {
       this.featureDetect.inputtypes.range = false;
       this.rangeInput = new this.RangeInput(this.$html);
       this.rangeInput.init();
@@ -98,7 +98,7 @@ describe('Range input', function () {
       this.$inputSlider = this.$html.find('.form__input-range');
     });
 
-    it('keeps the text input in sync if the slider changes', function () {
+    it('keeps the text input in sync if the slider changes', function() {
       this.$inputSlider.val('2000').trigger('change');
       expect(this.$inputText.val()).to.equal('1500');
     });
