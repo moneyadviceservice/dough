1a43ecb7148812a2b6bd39a3cbe1f7193a0ac99e
Ben Barnett <ben.barnett@moneyadviceservice.org.uk>
2014-08-05 11:50:22 +0100

    Fleshing component methods out and adding aria-required

--- assets/js/components/Validation.js
+++ assets/js/components/Validation.js
@@ -13,16 +13,26 @@
 define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
   'use strict';
 
-  var defaultConfig = {},
+  var defaultConfig = {
+    invalidClass: 'is-invalid',
+    validClass: 'is-valid',
+    rowInvalidClass: 'is-errored'
+  },
+
+  uiEvents = {
+    'blur input': '_handleBlurEvent',
+    'keyup input': '_handleKeyUpEvent',
+    'submit': '_handleSubmit'
+  },
 
     /**
    * Call base constructor
    * @constructor
    */
   Validation = function($el, config) {
+    this.uiEvents = uiEvents;
     Validation.baseConstructor.apply(this, arguments);
     this.config = $.extend(defaultConfig, this.config);
-    this.debounceTimer = null;
     this.init();
   };
 
@@ -31,6 +41,31 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
   Validation.prototype.init = function() {
     return this;
   };
+
+  /**
+   * Inline errors are shown on input blur
+   * @return {[type]} [description]
+   */
+  Validation.prototype._handleBlurEvent = function() {
+
+  };
+
+  /**
+   * Error messages get corrected as the user types
+   * @return {[type]} [description]
+   */
+  Validation.prototype._handleKeyUpEvent = function() {
+
+  };
+
+  /**
+   * The validation summary is updated on form submit
+   * @return {[type]} [description]
+   */
+  Validation.prototype._handleSubmit = function() {
+
+  };
+
 
   return Validation;
 
