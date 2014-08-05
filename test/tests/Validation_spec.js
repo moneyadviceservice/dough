fad9d303124b026b14008680a5389e5d9243f622
Ben Barnett <ben.barnett@moneyadviceservice.org.uk>
2014-08-05 10:01:12 +0100

    Adding fixtures and basic feature tests

--- test/tests/Validation_spec.js
+++ test/tests/Validation_spec.js
@@ -2,20 +2,81 @@ describe('Validation', function() {
 
   'use strict';
 
-  beforeEach(function(done) {
-    var self = this;
-    requirejs(
-        ['jquery', 'Validation'],
-        function($, Validation) {
-          self.$html = $(window.__html__['test/fixtures/Validation.html']).appendTo('body');
-          self.component = self.$html.find('[data-dough-component="Validation"]');
-          self.Validation = Validation;
-          done();
-        }, done);
+  // Basic required field
+  describe('NonEmpty', function() {
+    beforeEach(function(done) {
+      var self = this;
+      requirejs(
+          ['jquery', 'Validation'],
+          function($, Validation) {
+            self.$html = $(window.__html__['test/fixtures/Validation/NonEmpty.html']).appendTo('body');
+            self.component = self.$html.find('[data-dough-component="Validation"]');
+            self.Validation = Validation;
+            done();
+          }, done);
+    });
+
+    afterEach(function() {
+      this.$html.remove();
+    });
+
+    it('should show an inline error if left empty on blur', function() {
+
+    });
+
+    it('should show the validation summary if left empty on submit', function() {
+
+    });
+
+    it('should remove all relevant errors if corrected on key up', function() {
+
+    });
+
+    it('should allow the form to submit if the value is non-empty', function() {
+
+    });
   });
 
-  afterEach(function() {
-    this.$html.remove();
+
+
+  // Minimum length check
+  describe('MinLength and NonEmpty', function() {
+    beforeEach(function(done) {
+      var self = this;
+      requirejs(
+          ['jquery', 'Validation'],
+          function($, Validation) {
+            self.$html = $(window.__html__['test/fixtures/Validation/MinLength.html']).appendTo('body');
+            self.component = self.$html.find('[data-dough-component="Validation"]');
+            self.Validation = Validation;
+            done();
+          }, done);
+    });
+
+    afterEach(function() {
+      this.$html.remove();
+    });
+
+    it('should show an inline error if left empty on blur', function() {
+
+    });
+
+    it('should show the validation summary if left empty on submit', function() {
+
+    });
+
+    it('should remove all relevant errors if corrected on key up', function() {
+
+    });
+
+    it('should show an inline error if the value is not enough characters', function() {
+
+    });
+
+    it('should allow the form to submit if the value is non-empty and enough characters', function() {
+
+    });
   });
+
 
 });
