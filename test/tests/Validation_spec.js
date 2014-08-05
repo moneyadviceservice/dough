c7befb46ef90c52c43e52b9142073a3647c6eaf3
Ben Barnett <ben.barnett@moneyadviceservice.org.uk>
2014-08-05 10:11:11 +0100

    Basic tests for number and regexp

--- test/tests/Validation_spec.js
+++ test/tests/Validation_spec.js
@@ -83,4 +83,75 @@ describe('Validation', function() {
   });
 
 
+  // Regular expression check
+  describe('RegExp', function() {
+    beforeEach(function(done) {
+      var self = this;
+      requirejs(
+          ['jquery', 'Validation'],
+          function($, Validation) {
+            self.$html = $(window.__html__['test/fixtures/Validation/RegExp.html']).appendTo('body');
+            self.component = self.$html.find('[data-dough-component="Validation"]');
+            self.Validation = Validation;
+            done();
+          }, done);
+    });
+
+    afterEach(function() {
+      this.$html.remove();
+    });
+
+    it('should show an inline error if the regexp does not match on blur', function() {
+
+    });
+
+    it('should show the validation summary if the regexp does not match on submit', function() {
+
+    });
+
+    it('should remove all relevant errors if corrected on key up', function() {
+
+    });
+
+    it('should allow the form to submit if the value matches the regexp', function() {
+
+    });
+  });
+
+
+  // Number range check
+  describe('NumberRange', function() {
+    beforeEach(function(done) {
+      var self = this;
+      requirejs(
+          ['jquery', 'Validation'],
+          function($, Validation) {
+            self.$html = $(window.__html__['test/fixtures/Validation/NumberRange.html']).appendTo('body');
+            self.component = self.$html.find('[data-dough-component="Validation"]');
+            self.Validation = Validation;
+            done();
+          }, done);
+    });
+
+    afterEach(function() {
+      this.$html.remove();
+    });
+
+    it('should show an inline error if the value is out of range on blur', function() {
+
+    });
+
+    it('should show the validation summary if the value is out of range on submit', function() {
+
+    });
+
+    it('should remove all relevant errors if corrected on key up', function() {
+
+    });
+
+    it('should allow the form to submit if the value is in range', function() {
+
+    });
+  });
+
 });
