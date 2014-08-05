9b979f9e41eefe107f74366e85eebc5ec3660d4e
Ben Barnett <ben.barnett@moneyadviceservice.org.uk>
2014-08-05 10:04:29 +0100

    Empty tests for minlength not required

--- test/tests/Validation_spec.js
+++ test/tests/Validation_spec.js
@@ -65,6 +65,10 @@ describe('Validation', function() {
 
     });
 
+    it('should show the validation summary if not enough characters on submit', function() {
+
+    });
+
     it('should remove all relevant errors if corrected on key up', function() {
 
     });
@@ -74,6 +78,47 @@ describe('Validation', function() {
     });
 
     it('should allow the form to submit if the value is non-empty and enough characters', function() {
+
+    });
+  });
+
+
+
+  // Not required, but if present, validate length check
+  describe('MinLength not required', function() {
+    beforeEach(function(done) {
+      var self = this;
+      requirejs(
+          ['jquery', 'Validation'],
+          function($, Validation) {
+            self.$html = $(window.__html__['test/fixtures/Validation/MinLengthNotRequired.html']).appendTo('body');
+            self.component = self.$html.find('[data-dough-component="Validation"]');
+            self.Validation = Validation;
+            done();
+          }, done);
+    });
+
+    afterEach(function() {
+      this.$html.remove();
+    });
+
+    it('should NOT show an inline error if left empty on blur', function() {
+
+    });
+
+    it('should NOT show the validation summary if left empty on submit', function() {
+
+    });
+
+    it('should remove all relevant errors if corrected on key up', function() {
+
+    });
+
+    it('should show an inline error if the value is not enough characters', function() {
+
+    });
+
+    it('should allow the form to submit if the value is empty OR has enough characters', function() {
 
     });
   });
