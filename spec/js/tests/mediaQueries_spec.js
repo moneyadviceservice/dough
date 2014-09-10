7a8abd207d611c34818c9a679c27dcd71768d2dc
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2014-09-10 17:45:31 +0100

    mediaQueries - initialise when AMD module loads

--- spec/js/tests/mediaQueries_spec.js
+++ spec/js/tests/mediaQueries_spec.js
@@ -10,7 +10,7 @@ describe('mediaQueries - fires JS events when breakpoints are crossed', function
         function($, mediaQueries, eventsWithPromises) {
           self.mediaQueries = mediaQueries;
           self.eventsWithPromises = eventsWithPromises;
-          self.$html = $('<div />');
+          self.$html = $('<div />').appendTo('body');
           done();
         }, done);
   });
@@ -20,28 +20,18 @@ describe('mediaQueries - fires JS events when breakpoints are crossed', function
   });
 
   it('places a media query test element in the DOM', function() {
-    this.mediaQueries.init(this.$html);
-    expect(this.$html.children('.js-mediaquery-test').length).to.equal(1);
+    expect($('body').children('.js-mediaquery-test').length).to.equal(1);
   });
 
   it('fires a resize event on load', function() {
     var spy = sinon.spy();
     this.eventsWithPromises.subscribe('mediaquery:resize', spy);
-    this.mediaQueries.init(this.$html);
     expect(spy).to.have.been.called;
-  });
-
-  it('doesn\'t re-add the element if initialised twice', function() {
-    this.mediaQueries.init(this.$html);
-    this.mediaQueries.init(this.$html);
-    expect(this.$html.children('.js-mediaquery-test').length).to.equal(1);
   });
 
   it('only fires one resize event if initialised twice', function() {
     var spy = sinon.spy();
     this.eventsWithPromises.subscribe('mediaquery:resize', spy);
-    this.mediaQueries.init(this.$html);
-    this.mediaQueries.init(this.$html);
     spy.should.have.been.calledOnce;
   });
 
