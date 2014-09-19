58a1dd16f26e158423235342c28c54f459681078
Phil Lee <asmega@ph-lee.com>
2014-09-19 15:39:14 +0100

    add dough assets to precompile list

--- lib/dough/engine.rb
+++ lib/dough/engine.rb
@@ -7,5 +7,12 @@ module Dough
     config.to_prepare do
       ::ApplicationController.helper(Helpers)
     end
+
+    initializer "dough.assets.precompile" do |app|
+      app.config.assets.precompile += %w(dough/assets/stylesheets/basic.css
+                                         dough/assets/stylesheets/font_files.css
+                                         dough/assets/stylesheets/font_base64.css)
+      app.config.assets.precompile << /\.(?:png|svg|eot|woff|ttf)$/
+    end
   end
 end
