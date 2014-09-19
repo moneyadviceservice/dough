a4c2dc0333421a42e074cf67a88090efb1c50cf9
Phil Lee <asmega@ph-lee.com>
2014-09-19 12:57:20 +0100

    add working dough components documentation

---
+++ made_with_dough/app/controllers/home_controller.rb
@@ -0,0 +1,39 @@
+class HomeController < ApplicationController
+  def index
+    @components = Dir['../docs/*.md'].map{|path| Component.new(path)}
+  end
+
+  class Component
+    attr_reader :path
+
+    def initialize(path)
+      @path = path
+    end
+
+    def code_path
+      "../docs/helpers/#{name.camelize}.erb"
+    end
+
+    def code_raw_html
+      File.read code_path
+    end
+
+    def code_processed_html
+      ERB.new(code_raw_html)
+    end
+
+    def name
+      path.match(/\.\.\/docs\/(.*)\.md/)[1].underscore
+    end
+
+    def doc_markdown
+      File.read path
+    end
+
+    def doc_html
+      renderer = Redcarpet::Render::HTML
+      parser = Redcarpet::Markdown.new(renderer)
+      parser.render doc_markdown
+    end
+  end
+end
