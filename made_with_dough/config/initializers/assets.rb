37cce428e819ce20b1e27c55077159c7438d4209
Phil Lee <asmega@ph-lee.com>
2014-09-19 15:41:37 +0100

    load dough assets into docs app

--- made_with_dough/config/initializers/assets.rb
+++ made_with_dough/config/initializers/assets.rb
@@ -6,3 +6,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( enhanced.css )
+Rails.application.config.assets.precompile += %w( enhanced_fixed.css )
+Rails.application.config.assets.precompile += %w( enhanced_responsive.css )
