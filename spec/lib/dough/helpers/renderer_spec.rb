1829096894e90b081780a2f1ee8e384f4bbe2762
Yomi Colledge <baphled@boodah.net>
2014-10-06 15:42:54 +0100

    Describe the callout_editorial helper

--- spec/lib/dough/helpers/renderer_spec.rb
+++ spec/lib/dough/helpers/renderer_spec.rb
@@ -9,60 +9,128 @@ module Dough
     describe Renderer, type: :controller do
       render_views
 
-      controller do
-        helper Dough::Helpers
 
-        def index
-          render(inline: "<%= inset_block 'Some instructional text' %>")
-        end
-      end
-
-      describe '#inset_block' do
-        before :each do
-          get :index
-        end
-
-        it "renders text" do
-          expect(response.body).to include('Some instructional text')
-        end
-
-        it "has an inset_block class" do
-          expect(response.body).to include('class="inset-block"')
-        end
-
-        it 'has an inset_block content container class' do
-          expect(response.body).to include('class="inset-block__content-container"')
-        end
-
-        it 'has an inset_block text class' do
-          expect(response.body).to include('class="inset-block__text"')
-        end
-      end
-
-      describe '#callout_instructional' do
+      describe "#inset_block" do
         controller do
           helper Dough::Helpers
 
           def index
-            render(inline: "<%= callout_instructional html_content: {
+            render(inline: "<%= inset_block 'Some instructional text' %>")
+          end
+        end
+
+        describe "#render" do
+          context "helper template found" do
+            controller do
+              helper Dough::Helpers
+
+              def index
+                render(inline: "<%= inset_block 'Some instructional text' %>")
+              end
+            end
+            it "renders the template" do
+              get :index
+
+              expect(response.body).to include('Some instructional text')
+            end
+          end
+
+          context "helper template not found" do
+            controller do
+              helper Dough::Helpers
+
+              def index
+                render(inline: "<%= foo 'Some instructional text' %>")
+              end
+            end
+
+            it "throws an exception" do
+              expect {
+                get :index
+              }.to raise_error ActionView::Template::Error
+            end
+          end
+        end
+
+        describe "#inset_block" do
+          before :each do
+            get :index
+          end
+
+          it "renders text" do
+            expect(response.body).to include('Some instructional text')
+          end
+
+          it "has an inset_block class" do
+            expect(response.body).to include('class="inset-block"')
+          end
+
+          it "has an inset_block content container class" do
+            expect(response.body).to include('class="inset-block__content-container"')
+          end
+
+          it "has an inset_block text class" do
+            expect(response.body).to include('class="inset-block__text"')
+          end
+        end
+
+        describe "#callout_editorial" do
+          controller do
+            helper Dough::Helpers
+
+            def index
+              render(inline: "<%= callout_editorial 'Some heading' %>")
+            end
+          end
+
+
+          it 'renders "text"' do
+            get :index
+
+            expect(response.body).to include('Some heading')
+          end
+
+          it "wraps the text in a div element" do
+            get :index
+
+            expect(response.body).to include('<div class="callout-editorial">')
+          end
+
+          context "parsing html content" do
+
+            it "passed html heading is accessible" do
+              get :index
+
+              expect(response.body).to include('Some heading')
+            end
+          end
+        end
+
+        describe '#callout_instructional' do
+          controller do
+            helper Dough::Helpers
+
+            def index
+              render(inline: "<%= callout_instructional html_content: {
     heading: '<h3>Budgeting tips</h3>',
     content: '<p>In 1985, average first-time buyers needed a deposit of 5% to buy a home - in 2012, this had increased to 20%
     <br/><strong>Source: HM Treasury </strong>
       </p>'
 } %>")
+            end
           end
-        end
 
-        before :each do
-          get :index
-        end
+          before :each do
+            get :index
+          end
 
-        it 'renders html content' do
-          expect(response.body).to include('Budgeting tips')
-        end
+          it 'renders html content' do
+            expect(response.body).to include('Budgeting tips')
+          end
 
-        it 'wraps the text in a div element' do
-          expect(response.body).to include('<div class="callout-instructional">')
+          it 'wraps the text in a div element' do
+            expect(response.body).to include('<div class="callout-instructional">')
+          end
         end
       end
     end
