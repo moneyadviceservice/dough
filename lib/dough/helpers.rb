98a6be2e5048a38886075fff0d893fe5da6c2d01
Yomi Colledge <baphled@boodah.net>
2014-10-07 11:59:26 +0100

    Update code to use new Dough::Helpers::Renderer to handle our callout_editorial helper

--- lib/dough/helpers.rb
+++ lib/dough/helpers.rb
@@ -1,4 +1,3 @@
-require "dough/helpers/callout_editorial"
 require "dough/helpers/form_row"
 require "dough/helpers/renderer"
 
@@ -9,10 +8,6 @@ module Dough
     def method_missing(method_name, *args, &block)
       helper = "Dough::Helpers::Renderer".constantize
       helper.new(helper_name: method_name.to_s, renderer: self, text: args).render
-    end
-
-    def callout_editorial(text)
-      Helpers::CalloutEditorial.new(renderer: self, text: text).render
     end
   end
 end
--- spec/lib/dough/helpers/renderer_spec.rb
+++ spec/lib/dough/helpers/renderer_spec.rb
@@ -9,15 +9,16 @@ module Dough
     describe Renderer, type: :controller do
       render_views
 
-      controller do
-        helper Dough::Helpers
-
-        def index
-          render(inline: "<%= inset_block 'Some instructional text' %>")
-        end
-      end
 
       describe "#inset_block" do
+        controller do
+          helper Dough::Helpers
+
+          def index
+            render(inline: "<%= inset_block 'Some instructional text' %>")
+          end
+        end
+
         before :each do
           get :index
         end
@@ -32,6 +33,28 @@ module Dough
 
         it "has an inset_block text class" do
           expect(response.body).to include('class="inset-block__text"')
+        end
+      end
+
+      describe "#callout_editorial" do
+        controller do
+          helper Dough::Helpers
+
+          def index
+            render(inline: "<%= callout_editorial 'hello' %>")
+          end
+        end
+
+        it 'renders "text"' do
+          get :index
+
+          expect(response.body).to include('hello')
+        end
+
+        it "wraps the text in a div element" do
+          get :index
+
+          expect(response.body).to include('<div class="callout-editorial">')
         end
       end
     end
