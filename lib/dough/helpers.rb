f9093d2798f771f05a10437e883e2f83a1e322d5
Yomi Colledge <baphled@boodah.net>
2014-10-08 11:55:27 +0100

    Describe how to use the html_content option.
    Used callout_editorial to drive out functionality but may need to
    swap with a helper than actually uses that option.

--- lib/dough/helpers.rb
+++ lib/dough/helpers.rb
@@ -6,8 +6,11 @@ require 'active_support/core_ext'
 module Dough
   module Helpers
     def method_missing(method_name, *args, &block)
-      helper = 'Dough::Helpers::Renderer'.constantize
-      helper.new(helper_name: method_name.to_s, renderer: self, text: args).render
+      helper = "Dough::Helpers::Renderer".constantize
+      text, optional_args = *args
+      options = { helper_name: method_name.to_s, renderer: self, text: text }
+      options.merge! optional_args if optional_args
+      helper.new(options).render
     end
   end
 end
--- lib/dough/helpers/renderer.rb
+++ lib/dough/helpers/renderer.rb
@@ -1,17 +1,18 @@
 module Dough
   module Helpers
     class Renderer
-      attr_reader :text, :renderer, :helper_name
+      attr_reader :text, :renderer, :helper_name, :html_content
 
       def initialize(options = {})
         @helper_name = options[:helper_name]
         @text = options[:text]
         @renderer = options[:renderer]
+        @html_content = options[:html_content]
       end
 
       def render
         template_file = "dough/helpers/#{helper_name}/#{helper_name}"
-        context = renderer.render(partial: template_file, locals: { text: text })
+        context = renderer.render(partial: template_file, locals: { text: text, html_content: html_content })
         if context.empty?
           fail NameError
         else
