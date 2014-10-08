6299660706e8e659cbb5d2e71ef95331a8cff878
Yomi Colledge <baphled@boodah.net>
2014-10-08 11:55:27 +0100

    Describe how to use the html_content option.
    Used callout_editorial to drive out functionality but may need to
    swap with a helper than actually uses that option.

--- lib/dough/helpers.rb
+++ lib/dough/helpers.rb
@@ -7,7 +7,10 @@ module Dough
   module Helpers
     def method_missing(method_name, *args, &block)
       helper = "Dough::Helpers::Renderer".constantize
-      helper.new(helper_name: method_name.to_s, renderer: self, text: args).render
+      text, optional_args = *args
+      options = { helper_name: method_name.to_s, renderer: self, text: text }
+      options.merge! optional_args if optional_args
+      helper.new(options).render
     end
   end
 end
