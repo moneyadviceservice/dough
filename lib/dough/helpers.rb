ae82f43bbc5cfa7fc167cfa32d310b20941bbaf8
Yomi Colledge <baphled@boodah.net>
2014-10-14 11:18:44 +0100

    Improve the readability of Dough::Helpers

--- lib/dough/helpers.rb
+++ lib/dough/helpers.rb
@@ -6,31 +6,33 @@ require 'active_support/core_ext'
 module Dough
   module Helpers
     def method_missing(method_name, *args, &block)
-      helper = "Dough::Helpers::Renderer".constantize
       if args.first.class == String
-        options = helper_with_text method_name, *args
+        helper_with_text(method_name, *args)
       elsif args.first.class == Hash
-        options = helper_without_text method_name, *args
+        helper_without_text(method_name, *args)
       else
         super
       end
-      helper.new(options).render
     end
 
     private
+
+    def helper
+      "Dough::Helpers::Renderer".constantize
+    end
 
     def helper_with_text method_name, *args
       text, optional_args = *args
       options = { helper_name: method_name.to_s, renderer: self, text: text }
       options.merge! optional_args if optional_args
-      options
+      helper.new(options).render
     end
 
     def helper_without_text method_name, *args
       optional_args = *args
       options = { helper_name: method_name.to_s, renderer: self }
-      optional_args.each { |arg| options.merge! arg } if optional_args
-      options
+      optional_args.each { |option| options.merge! option } if optional_args
+      helper.new(options).render
     end
   end
 end
