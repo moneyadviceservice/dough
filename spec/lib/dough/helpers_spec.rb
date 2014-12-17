f91b16c3eee511a5786d4c989e15d03b207aaef8
Tomas D'Stefano <tomas_stefano@successoft.com>
2014-12-17 15:05:38 +0000

    Add specs to heading_tag helper method

--- spec/lib/dough/helpers_spec.rb
+++ spec/lib/dough/helpers_spec.rb
@@ -3,8 +3,11 @@ require 'spec_helper'
 describe Dough::Helpers do
   class Foo
     include Dough::Helpers
+    include ActionView::Helpers::TagHelper
+    include ActionView::Helpers::CaptureHelper
 
     attr_reader :text, :renderer
+    attr_accessor :output_buffer
 
     def initialize(options = {})
       @text = options[:text]
@@ -18,6 +21,40 @@ describe Dough::Helpers do
 
   let(:renderer) { double(:Renderer, render: render) }
   subject(:helper) { Foo.new(helper_name: :inset_block, renderer: renderer, text: 'foo') }
+
+  describe '#heading_tag' do
+    let(:render) { double(:render, render: '', empty?: false) }
+
+    context 'when passing a block' do
+      subject(:heading_tag) do
+        helper.heading_tag { 'Header' }
+      end
+
+      it 'returns the header with content in the block' do
+        expect(heading_tag).to eq('<h1 aria-level="1" role="heading">Header</h1>')
+      end
+    end
+
+    context 'when passing different heading levels' do
+      subject(:heading_tag) do
+        helper.heading_tag('Header', level: 2)
+      end
+
+      it 'returns the level passed in the options' do
+        expect(heading_tag).to eq('<h2 aria-level="2" role="heading">Header</h2>')
+      end
+    end
+
+    context 'when not passing the heading level' do
+      subject(:heading_tag) do
+        helper.heading_tag('Header')
+      end
+
+      it 'returns h1 as default' do
+        expect(heading_tag).to eq('<h1 aria-level="1" role="heading">Header</h1>')
+      end
+    end
+  end
 
   describe '#method_missing' do
     context 'helper found' do
