d1069fa5ab795d4d64f8ec04d1588f61a1780870
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2015-05-07 15:16:56 +0100

    Fixes build script
    * adds missing `npm install` which was causing build failures.
    * references gulp build task
    * fixes app:gem:build issue @tomasdestefi

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ set -e -x
 export RAILS_ENV=build
 export BUNDLE_WITHOUT="development:test"
 
-gulp build
+npm install
+./node_modules/gulp/bin/gulp.js build
 
 time bundle install
-time bundle exec rake app:gem:build
+time bundle exec rake build
