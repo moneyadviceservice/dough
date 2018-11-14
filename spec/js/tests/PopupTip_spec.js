91a103057b229dad43eb24e4caaa6f8150abce7c
David Trussler <mail@davidtrussler.net>
2018-11-14 16:22:53 +0000

    Updates Karma to allow getting of element positions

--- spec/js/tests/PopupTip_spec.js
+++ spec/js/tests/PopupTip_spec.js
@@ -1,65 +1,52 @@
 describe.only('Displays Popup Tooltip', function() {
   'use strict';
 
-  // var webPage = require('webpage');
-  // console.log(webPage);
-
   var activeClass   = 'is-active',
       inactiveClass = 'is-inactive';
 
   beforeEach(function(done) {
     var self = this;
 
+    fixture.setBase('spec/js/fixtures');
+
     requirejs(
       ['jquery', 'PopupTip'], function($, PopupTip) {
-        self.$html    = $(window.__html__['spec/js/fixtures/PopupTip.html']).appendTo('body');
-        self.PopupTip = PopupTip;
-        // self.webPage = require('webpage');
+        fixture.load('PopupTip.html');
+
+        self.$popupTip = $(fixture.el).find('[data-dough-component="PopupTip"]');
+        self.obj = new PopupTip(self.$popupTip);
+        self.$trigger   = self.$popupTip.find('[data-dough-popup-trigger]');
+        self.$container = self.$popupTip.find('[data-dough-popup-container]');
+        self.$close     = self.$popupTip.find('[data-dough-popup-close]');
+
+        self.obj.init();
 
         done();
       }, done);
   });
 
   afterEach(function() {
-    this.$html.remove();
+    fixture.cleanup();
   });
 
   describe('Default behaviour', function() {
-
-    beforeEach(function(done) {
-      this.$html      = $(window.__html__['spec/js/fixtures/PopupTip.html']);
-      this.$trigger   = this.$html.find('[data-dough-popup-trigger]');
-      this.$container = this.$html.find('[data-dough-popup-container]');
-      this.$close     = this.$html.find('[data-dough-popup-close]');
-      this.popupTip   = new this.PopupTip(this.$html);
-      this.popupTip.init();
-
-      // this.page = this.webPage.create();
-
-      // console.log(page);
-
-      done();
-    });
-
     it('hides the popup content on load', function() {
       expect(this.$container).to.not.have.class(activeClass);
     });
 
     it('displays the popup in the correct position on trigger click', function() {
+      var trigger = this.$trigger[0];
+
       this.$trigger.click();
       expect(this.$container).to.have.class(activeClass);
 
-      var trigger = this.$trigger[0];
       console.log(trigger.getBoundingClientRect());
       console.log($(window).width());
-      // console.log($(trigger).width());
     });
 
     it('closes the popup on close button click', function() {
       this.$close.click();
       expect(this.$container).to.have.class(inactiveClass);
     });
-
   });
-
 });
