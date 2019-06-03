807fb592759fd1d877791e8b8b78589f476980bd
Andre Ferreira <43468798+andreferreiradlw@users.noreply.github.com>
2019-06-03 11:00:55 +0100

    [DEV APPROVED] 10485 popup channel select (#330)
    * Setup initial event listeners
    
    * Select Event Listener
    
    * Implemented JS Testing
    
    * Select Validation changed to clumps id
    
    * Class name corrections
    
    * Element selector changes
    
    * Modified raisedChatPopup Method

--- assets/js/components/ChatPopup.js
+++ assets/js/components/ChatPopup.js
@@ -1,38 +1,89 @@
 define(['jquery', 'DoughBaseComponent'],
-  function($, DoughBaseComponent) {
-  'use strict';
+  function ($, DoughBaseComponent) {
+    'use strict';
 
-  var ChatPopup,
+    var ChatPopup,
       defaultConfig = {};
 
-  ChatPopup = function($el, config) {
-    ChatPopup.baseConstructor.call(this, $el, config, defaultConfig);
+    ChatPopup = function ($el, config) {
+      ChatPopup.baseConstructor.call(this, $el, config, defaultConfig);
 
-    this.chatPopupBtn = $el;
-  };
+      this.chatPopupBtn = $el;
+      this.chatPopupIcon = $el.find('[data-dough-webchat-icon]');
+      this.chatPopupClose = $el.find('[data-dough-webchat-close]');
+      this.popupElements = $el.children().not('[data-dough-webchat-icon]');
+      this.serviceSelect = $el.find('[data-dough-webchat-select]');
+      this.whatsappBtn = $el.find('[data-dough-webchat-button-whatsapp]');
+      this.webchatBtn = $el.find('[data-dough-webchat-button-webchat]');
+    };
 
-  /**
-  * Inherit from base module, for shared methods and interface
-  */
-  DoughBaseComponent.extend(ChatPopup);
+    /**
+     * Inherit from base module, for shared methods and interface
+     */
+    DoughBaseComponent.extend(ChatPopup);
 
-  ChatPopup.componentName = 'ChatPopup';
+    ChatPopup.componentName = 'ChatPopup';
 
-  /**
-   * Public method imported in BackToTop.js to manage popup vertical position in article pages
-   * @param {boolean} raised - set button raised state
-   * @param {boolean} atSmallViewport - viewport width < 720px
-   */
-  ChatPopup.prototype._raisedChatPopup = function(raised, atSmallViewport) {
-    raised && atSmallViewport ? this.chatPopupBtn.addClass('chat-popup--raised') : this.chatPopupBtn.removeClass('chat-popup--raised');
-  }
+    ChatPopup.prototype._setupListeners = function () {
+      var self = this;
+      // on icon click open popup
+      this.chatPopupIcon.click(function (event) {
+        event.preventDefault();
+        self._togglePopup();
+        self._manageTransition(1);
+      });
+      // on X click close popup
+      this.chatPopupClose.click(function (event) {
+        event.preventDefault();
+        self._togglePopup();
+        self._manageTransition(0);
+      });
+      // on select change
+      this.serviceSelect.change(function(event) {
+        if(event.target.value === 'debt-and-borrowing' || event.target.value === 'pensions-and-retirement') {
+          self.whatsappBtn.removeClass('is-hidden');
+        } else {
+          self.whatsappBtn.addClass('is-hidden');
+        }
+      });
+    };
 
-  /**
-  * @param {Promise} initialised
-  */
- ChatPopup.prototype.init = function(initialised) {
-    this._initialisedSuccess(initialised);
-  };
+    ChatPopup.prototype._manageTransition = function (opacity) {
+      var self = this;
+      setTimeout(function () {
+        $.each(self.popupElements, function (index, value) {
+          value.style.opacity = opacity;
+          value.style.filter = "alpha(opacity=" + opacity + "00)"; // IE fallback
+        });
+      }, 100);
+    };
 
-  return ChatPopup;
-});
+    ChatPopup.prototype._togglePopup = function () {
+      this.chatPopupBtn.toggleClass('mobile-webchat--opened').toggleClass('mobile-webchat--closed');
+    };
+
+    /**
+     * Public method imported in BackToTop.js to manage popup vertical position in article pages
+     * @param {boolean} raised - set button raised state
+     * @param {boolean} atSmallViewport - viewport width < 720px
+     */
+    ChatPopup.prototype._raisedChatPopup = function (raised, atSmallViewport) {
+      var raisedPopup = raised && atSmallViewport;
+      // check if conditions are met
+      if (raisedPopup) {
+        this.chatPopupBtn.addClass('mobile-webchat--raised')
+      } else {
+        this.chatPopupBtn.removeClass('mobile-webchat--raised')
+      };
+    }
+
+    /**
+     * @param {Promise} initialised
+     */
+    ChatPopup.prototype.init = function (initialised) {
+      this._setupListeners();
+      this._initialisedSuccess(initialised);
+    };
+
+    return ChatPopup;
+  });
