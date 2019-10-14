b8b1d649824acc153840c73a66c099eabee9b4bf
Malcolm Baig <malcolm.baig@gmail.com>
2019-10-14 11:51:22 +0100

    Changes to support Rails 5
    * Remove upper version constraint.
      - We shouldn't be applying this anyway, as individual host apps should
        have the responsibility of ensuring compatibility with Dough as part
        of their upgrade process.
    * Remove assets.precompile config.
       - Rails 5 uses Sprockets 4. This new version of Sprockets encourages
         the use of manifest files to describe which assets should be
         precompiled.
       - Sprockets 4 doesn't support regex values in this config.
    * Rename lib/dough.rb to lib/dough-ruby.rb.
       - Rails 5 doesn't load this engine (eg - various classes and modules are
         missing) without this name change, possibly due to changes in how
         engines work between Rails 4 and 5.

--- lib/dough/engine.rb
+++ lib/dough/engine.rb
@@ -9,10 +9,10 @@ module Dough
     end
 
     initializer 'dough.assets.precompile' do |app|
-      app.config.assets.precompile += %w[dough/assets/stylesheets/basic.css
-                                         dough/assets/stylesheets/font_files.css
-                                         dough/assets/stylesheets/font_base64.css]
-      app.config.assets.precompile << /\.(?:png|svg|eot|woff|ttf)$/
+      # app.config.assets.precompile += %w[dough/assets/stylesheets/basic.css
+      #                                    dough/assets/stylesheets/font_files.css
+      #                                    dough/assets/stylesheets/font_base64.css]
+      # app.config.assets.precompile << /\.(?:png|svg|eot|woff|ttf)$/
     end
   end
 end
