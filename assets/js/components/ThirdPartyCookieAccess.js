acc3cd27ca05cd9dee6992ed828add44a89657af
David Trussler <mail@davidtrussler.net>
2021-02-17 11:45:30 +0000

    Adds cy translation to message

--- assets/js/components/ThirdPartyCookieAccess.js
+++ assets/js/components/ThirdPartyCookieAccess.js
@@ -7,33 +7,33 @@
  *     - if the user's device/browser does not have access to cookies/storage a link is rendered instead of the content
  */
 define(['ThirdPartyCookieAccess'], function(ThirdPartyCookieAccess) {
-  function ThirdPartyCookieAccess() {
+	function ThirdPartyCookieAccess() {
 		this.el = document.body; 
 		this.init(); 
-  };
+	};
 
-  ThirdPartyCookieAccess.prototype.init = function() {
-    this.hideContent(); 
-    this.queryDevice(); 
-  };
+	ThirdPartyCookieAccess.prototype.init = function() {
+		this.hideContent(); 
+		this.queryDevice(); 
+	};
 
-  ThirdPartyCookieAccess.prototype.queryDevice = function() {
+	ThirdPartyCookieAccess.prototype.queryDevice = function() {
 		var isStorageAccessKnown = this.isStorageAccessKnown(); 
-		
-    console.log('Determining if the `hasStorageAccess` method of the Storage Access API is known to this browser ...'); 
 
-    if (isStorageAccessKnown === false) {
-      console.log('`hasStorageAccess` IS NOT known to this browser!'); 
+		console.log('Determining if the `hasStorageAccess` method of the Storage Access API is known to this browser ...'); 
 
-    	this.showContent(); 
-    } else {
-      console.log('`hasStorageAccess` IS known to this browser!'); 
+		if (isStorageAccessKnown === false) {
+			console.log('`hasStorageAccess` IS NOT known to this browser!'); 
 
-    	this.hasAccess(); 
-    }; 
-  }; 
+			this.showContent(); 
+		} else {
+			console.log('`hasStorageAccess` IS known to this browser!'); 
 
-  ThirdPartyCookieAccess.prototype.hasAccess = function() {
+			this.hasAccess(); 
+		}; 
+	}; 
+
+	ThirdPartyCookieAccess.prototype.hasAccess = function() {
 		console.log('Determining if device has access to Storage/Cookies ...'); 
 
 		document.hasStorageAccess().then(
@@ -52,15 +52,15 @@ define(['ThirdPartyCookieAccess'], function(ThirdPartyCookieAccess) {
 			console.log('Could not determine if we have access!'); 
 			console.log(e); 
 		})
-  }; 
+	}; 
 
-  ThirdPartyCookieAccess.prototype.hideContent = function() {
-	  this.el.style.display = 'none'; 
-  }; 
+	ThirdPartyCookieAccess.prototype.hideContent = function() {
+		this.el.style.display = 'none'; 
+	}; 
 
-  ThirdPartyCookieAccess.prototype.showContent = function() {
+	ThirdPartyCookieAccess.prototype.showContent = function() {
 		this.el.style.display = 'block'; 
-  }; 
+	}; 
 
 	ThirdPartyCookieAccess.prototype.renderLink = function() {
 		var frame = window.frames,
@@ -69,21 +69,21 @@ define(['ThirdPartyCookieAccess'], function(ThirdPartyCookieAccess) {
 				message = document.createElement('div'), 
 				header, 
 				headerStyles = {
-					'margin-top': 0, 
-					'background-image': 'url(/assets/dough/assets/images/exclamation_mark.svg)', 
-					'background-repeat': 'no-repeat', 
-					'background-position': 'left center', 
-					'background-size': '2.25rem', 
+					'margin-top': 0,
+					'background-image': 'url(/assets/dough/assets/images/exclamation_mark.svg)',
+					'background-repeat': 'no-repeat',
+					'background-position': 'left center',
+					'background-size': '2.25rem',
 					'padding-left': '2.5rem'
-				}, 
+				},
 				messageHTML = {
 					en: `<h1>Oops!</h1>
 					<p>Sorry, it looks like your browser has blocked our tool. This is most likely due to improved security in relation to the use of cookies.</p>
 					<p>You can still load the tool on this browser by visiting the <a href="${href}" target="_blank" class="ThirdPartyCookieAccessReferLink">Money Advice Service tool page</a> (opens in a new window) or by visiting this page in a different browser.</p>`, 
-					cy: `<h1>Oops! &#x1F3F4&#xE0067&#xE0062&#xE0077&#xE006C&#xE0073&#xE007F;</h1>
-					<p>Sorry, it looks like your browser has blocked our tool. This is most likely due to improved security in relation to the use of cookies.</p>
-					<p>You can still load the tool on this browser by visiting the <a href="${href}" target="_blank" class="ThirdPartyCookieAccessReferLink">Money Advice Service tool page</a> (opens in a new window) or by visiting this page in a different browser.</p>`
-				}; 
+					cy: `<h1>Wps!</h1>
+					<p>Mae'n ddrwg gennym, mae'n edrych fel bod eich porwr wedi rhwystro ein teclyn. Mae hyn yn fwyaf tebygol oherwydd gwell diogelwch gan Apple mewn perthynas â defnyddio cwcis.</p>
+					<p>Gallwch ddal llwytho'r teclyn ar y porwr hwn trwy ymweld â <a href="${href}" target="_blank" class="ThirdPartyCookieAccessReferLink">thudalen teclynnau’r Gwasanaeth Cynghori Ariannol</a> (a fydd yn agor mewn ffenest newydd) neu trwy ymweld â'r dudalen hon mewn porwr gwahanol.</p>`
+				};
 
 		message.innerHTML = messageHTML[locale]; 
 
@@ -96,15 +96,15 @@ define(['ThirdPartyCookieAccess'], function(ThirdPartyCookieAccess) {
 		for (let style in headerStyles) {
 			header.style[style] = headerStyles[style];
 		}
-  }; 
+	}; 
 
-  ThirdPartyCookieAccess.prototype.isStorageAccessKnown = function() {
+	ThirdPartyCookieAccess.prototype.isStorageAccessKnown = function() {
 		if (document.hasStorageAccess === undefined) {
 			return false; 
 		} else {
 			return true; 
 		}; 
-  }; 
+	}; 
 
-  return ThirdPartyCookieAccess;
+	return ThirdPartyCookieAccess;
 });
